9fd016bed73e29d6f1a8d8584101b07de2b9f282
Jelle Herold <jelle@defekt.nl>
2019-07-29 00:53:09 +0200

    add jules texts

--- src/ui/pages/about-page.js
+++ src/ui/pages/about-page.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// https://hackmd.io/udwS94xLRXCSt1aE6YefvQ
+
 export default function About() {
   return (
     <div
@@ -9,49 +11,24 @@ export default function About() {
         justifyContent: "inter-word"
       }}
     >
-      <h2>About</h2>
-
+      <h2>Applied category theory</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-
-      <p>
-        Donec at tellus orci. Nulla enim mauris, vestibulum sed consequat nec,
-        aliquam ut erat. Curabitur tempor leo et neque venenatis aliquet.
-        Curabitur faucibus tortor eget felis vulputate, at elementum tortor
-        semper. Donec in pulvinar risus. Sed a nulla ac sapien varius dapibus.
-        Suspendisse eu ultricies magna. Etiam metus leo, vulputate convallis
-        rutrum et, venenatis malesuada arcu. Maecenas eu purus non leo fermentum
-        dignissim.
+      Applied category theory is the application of <i>category theory</i> (and related fields of pure mathematics) to other fields, especially in engineering, science and social science.
       </p>
       <p>
-        Quisque massa quam, pulvinar quis nulla ac, mattis malesuada tortor.
-        Nulla ac sollicitudin libero. Pellentesque ut aliquet eros. Donec
-        porttitor fringilla neque vel tristique. Vivamus non semper neque, sit
-        amet consequat tortor. Nam felis metus, porttitor a nisl eget, semper
-        rutrum ex. Morbi pharetra ac tortor ut sagittis. Sed dictum cursus nibh
-        vitae consequat. Nulla eget vehicula libero.
+        Applied category theory is quite a diverse field, but relatively common themes are a focus on <i>compositionality</i> to reason in a scalable way, and the use of <i>string diagrams</i> as a practical notation.
       </p>
       <p>
-        Nullam in nisi dignissim, euismod magna vitae, pulvinar urna. Integer
-        volutpat, justo eget egestas sollicitudin, urna leo mollis mi, ut
-        gravida enim metus convallis ipsum. Maecenas venenatis dolor vel nulla
-        elementum venenatis. Quisque pellentesque dui sit amet pharetra dapibus.
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed mattis
-        tellus vel semper feugiat. Duis varius purus eu lectus maximus feugiat.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-        hendrerit magna sed dictum tempus. Maecenas posuere felis felis, ac
-        porta sapien mattis non. Nunc eros elit, elementum eu porttitor ac,
-        lacinia tempus quam. Sed quis nibh enim. Morbi convallis dui a nisl
-        facilisis placerat. Praesent lacus risus, cursus porta ultricies id,
-        semper nec justo. Donec ac orci nisl. Vivamus sapien nibh, ornare ac
-        faucibus at, tempus nec erat.
+        Existing sub-fields of applied category theory include:
+        <ul>
+          <li>functional programming</li>
+          <li>programming language semantics</li>
+          <li>quantum computing</li>
+          <li>linguistics</li>
+          <li>open systems</li>
+          <li>machine learning</li>
+          <li>probability</li>
+        </ul>
       </p>
     </div>
   );
--- src/ui/pages/landing-page.js
+++ src/ui/pages/landing-page.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import { LoginLink } from '../routing.js'
+import { LoginLink } from "../routing.js";
 
-import Center from '../parts/center.js'
+import Center from "../parts/center.js";
+
+// https://hackmd.io/bvkJTESdTxumDaODt2lJPg?edit
 
 export default function Landing() {
-    return (
-      <Center>
-          <h1>ACT</h1>
-          <p>
-            TODO: Jules write
-          </p>
-          <LoginLink/>
-        </Center>
-    );
-  }
+  return (
+    <Center>
+      <h1>
+        What is <b>act.statebox.org</b>?
+      </h1>
+      <p>
+        This site serves two different (but related) purposes:
+        <ol>
+          <li>
+            It is a community hub and research wiki for the{" "}
+            <a href="/about">Applied category theory</a> community, just as the{" "}
+            <a href="https://ncatlab.org/nlab/show/HomePage">nLab</a> and <a href="https://nforum.ncatlab.org/">nForum</a> are for higher
+            category theory.
+          </li>
+          <li>
+            It is a place to collect more pedagogical material on category
+            theory that is simpler than you will find on the nLab, but more
+            in-depth than is suitable for Wikipedia
+          </li>
+        </ol>
+      </p>
+      <h1><LoginLink /></h1>
+    </Center>
+  );
+}
